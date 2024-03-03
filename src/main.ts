f6396491bf062737773c69966830169cb1b383b4
js-senpai <fincheran@gmail.com>
2024-03-03 18:51:45 +0200

    test commit

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,16 @@ import { WinstonModule } from 'nest-winston';
 import { winstonConfig } from './common/configs/winston.config';
 import { PrismaService } from './common/services/prisma.service';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { I18nValidationExceptionFilter, I18nValidationPipe } from 'nestjs-i18n';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useGlobalPipes(new I18nValidationPipe());
+
+  app.useGlobalFilters(
+    new I18nValidationExceptionFilter({
+      detailedErrors: false,
+    }),
+  );
   app.useLogger(WinstonModule.createLogger(winstonConfig));
   // Prisma
   const prismaService = app.get(PrismaService);
